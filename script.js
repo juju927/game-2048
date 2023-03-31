2302472932ac238449f8332d834f3cc5abcfd7fc
927joanna <sot.joanna@gmail.com>
2023-03-31 13:21:36 +0800

    added createNewTile()
    will add a 2 or 4 to a random empty grid box

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+// variables
 const gameGridEl = document.querySelector('.game-grid');
 const gridBoxes = document.querySelectorAll('.grid-box');
 
-console.log(gridBoxes);
-
-
-// to get empty grid boxes (filter out ones with tiles)
-  // ones with tiles need class grid-box-tile
-  // ones
-const emptyGridBoxes = [];
-for (gridBox of gridBoxes) {
-  if (gridBox.classList.contains("grid-box-tile")) {
-    console.log("blah");
-  } else {
-    emptyGridBoxes.push(gridBox);
-  }
+// functions
+function chooseNewTileNumber() {
+  return [2, 4][Math.round(Math.random())];
 }
-console.log(emptyGridBoxes);
+
+function createNewTile() {
+  // to get empty grid boxes (filter out ones with tiles)
+  // ones with tiles need class grid-box-tile
+  const emptyGridBoxes = [];
+  for (gridBox of gridBoxes) {
+    if (!(gridBox.classList.contains("grid-box-tile"))) {
+      emptyGridBoxes.push(gridBox);
+    }
+  };
+  const index = Math.floor(Math.random() * emptyGridBoxes.length);
+  
+  const elementToEdit = emptyGridBoxes[index];
+
+  elementToEdit.innerText = chooseNewTileNumber();
+  elementToEdit.classList.add("grid-box-tile");
+}
+
+setInterval(createNewTile, 1000);
+
+
+
+
+
+
+// event listeners

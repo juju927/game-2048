49fd0959bedfd37382052c55113f1d2c2074cfe4
927joanna <sot.joanna@gmail.com>
2023-03-31 13:52:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ const gridBoxes = document.querySelectorAll('.grid-box');
 
 // functions
 function chooseNewTileNumber() {
+  // return either 2 or 4
   return [2, 4][Math.round(Math.random())];
-}
+};
 
 function createNewTile() {
   // to get empty grid boxes (filter out ones with tiles)
-  // ones with tiles need class grid-box-tile
   const emptyGridBoxes = [];
   for (gridBox of gridBoxes) {
     if (!(gridBox.classList.contains("grid-box-tile"))) {
       emptyGridBoxes.push(gridBox);
     }
   };
-  const index = Math.floor(Math.random() * emptyGridBoxes.length);
-  
+  // get a random index from the array of empty grid boxes
+  const index = Math.floor(Math.random() * emptyGridBoxes.length); 
   const elementToEdit = emptyGridBoxes[index];
-
   elementToEdit.innerText = chooseNewTileNumber();
   elementToEdit.classList.add("grid-box-tile");
 }
 
-setInterval(createNewTile, 1000);
-
+function moveLeft() {
+  
+}
 
 
 

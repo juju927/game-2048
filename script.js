0c1589857a17d75179423f2b217715e71ab9c512
927joanna <sot.joanna@gmail.com>
2023-03-31 17:42:50 +0800

    finished push(direction)

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function createNewTile() {
   const elementToEdit = emptyGridBoxes[index];
   elementToEdit.innerText = chooseNewTileNumber();
   elementToEdit.classList.add("grid-box-tile");
-}
+};
 
 function moveBox(boxFrom, boxTo) { 
 // boxFrom -current box; boxTo -where it will move to
@@ -34,8 +34,8 @@ function moveBox(boxFrom, boxTo) {
   // --- under this line means boxFrom contains a number ---
   if (boxTo.classList.contains("grid-box-tile")) {
     // boxTo has number
-    if (!(boxTo.classList.contains("merged"))) {
-      // boxTo's number is not a merged one
+    if (!(boxTo.classList.contains("merged")) && !(boxFrom.classList.contains("merged"))) {
+      // boxTo's number & boxFrom's numbers are both not merged ones
       if (boxTo.innerText == boxFrom.innerText) {
         // boxTo's number is same as boxFrom's -> merge boxFrom's and boxTo's numbers
         // 1 - boxTo's number * 2
@@ -63,39 +63,99 @@ function moveBox(boxFrom, boxTo) {
     // 2 - move class grid-box-tile
     boxTo.classList.add("grid-box-tile");
     boxFrom.classList.remove("grid-box-tile");
+    
+    if (boxFrom.classList.contains("merged")) {
+      // if boxFrom was merged number
+      // 3 - move class merged
+      boxTo.classList.add("merged")
+      boxFrom.classList.remove("merged");
+    }
     return boxTo; 
   }
-}
+};
 
+function push(direction) {
+  // direction - "left", "right", "up", "down"
+  let axis = "";
+  let lastLine = "";
 
+  switch (direction) {
+    case "left":
+      axis = "row";
+      lastLine = "col1";
+      break;
 
-for (let rowNum = 1; rowNum <= 4; rowNum++) {
-  const row = document.querySelectorAll(".row"+rowNum);
-  console.log(row);
+    case "right":
+      axis = "row";
+      lastLine = "col4";
+      break;
 
-  for (let i = 1; i < row.length; i++) {
-    let j = i;
-    console.log(i);
-    while (!moveBox(row[j], row[j-1]).classList.contains("col1")) {
-      j--;
-      console.log(`from: ${row[j]} to: ${row[j-1]} j is ${j}`);
+    case "up":
+      axis = "col";
+      lastLine = "row1";
+      break;
+
+    case "down":
+      axis = "col";
+      lastLine = "row4";
+      break;
+  }
+  
+
+  for (let k = 1; k <= 4; k++) {
+    // get all divs in each row/ column (along pushed axis)
+    const rowOrCol = document.querySelectorAll("."+axis+k);
+
+    if (direction == "left" || direction == "up") {
+      for (let i = 1; i < rowOrCol.length; i++) {
+        let j = i;
+        while (!moveBox(rowOrCol[j], rowOrCol[j-1]).classList.contains(lastLine)) {
+          // while box hasn't moved to its final spot, move the same box again
+          j--
+        }
+      }
+    } else {
+      for (let i = 3; i > 0; i--) {
+        let j = i;
+        while (!moveBox(rowOrCol[j-1], rowOrCol[j]).classList.contains(lastLine)) {
+          // while box hasn't moved to its final spot, move the same box again
+          j++
+        }
+      }
     }
-  };
-}
+  }
+};
 
-for (let colNum = 1; colNum <= 4; colNum++) {
-  const col = document.querySelectorAll(".col"+colNum);
-  console.log(col);
+push("down");
 
-  for (let i = 1; i < col.length; i++) {
-    let j = i;
-    console.log(i);
-    while (!moveBox(col[j], col[j-1]).classList.contains("row1")) {
-      j--;
-      console.log(`from: ${col[j]} to: ${col[j-1]} j is ${j}`);
-    }
-  };
-}
+
+// for (let rowNum = 1; rowNum <= 4; rowNum++) {
+//   const row = document.querySelectorAll(".row"+rowNum);
+//   console.log(row);
+
+//   for (let i = 1; i < row.length; i++) {
+//     let j = i;
+//     console.log(i);
+//     while (!moveBox(row[j], row[j-1]).classList.contains("col1")) {
+//       j--;
+//       console.log(`from: ${row[j]} to: ${row[j-1]} j is ${j}`);
+//     }
+//   };
+// }
+
+// for (let colNum = 1; colNum <= 4; colNum++) {
+//   const col = document.querySelectorAll(".col"+colNum);
+//   console.log(col);
+
+//   for (let i = 1; i < col.length; i++) {
+//     let j = i;
+//     console.log(i);
+//     while (!moveBox(col[j], col[j-1]).classList.contains("row1")) {
+//       j--;
+//       console.log(`from: ${col[j]} to: ${col[j-1]} j is ${j}`);
+//     }
+//   };
+// }
 
 
 
